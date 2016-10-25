58aabf49cda8b02beb73ca0da8f70792071708a3
dujiand <dujiand@sfu.ca>
2016-10-24 20:40:58 -0700

    using degree heuristc as the tie-breaker of mrv

--- A2/FindNextVet_dgr.py
+++
@@ -1,9 +0,0 @@
-from heapq import heappop,heappush
-
-
-def FindNextVet(assignment, vetset, remainval, csp):
-    vetheap = list()
-    for vet in vetset:
-        if vet not in assignment.keys():
-            heappush(vetheap,(- len(csp[vet]),vet))
-    return heappop(vetheap)[1]
